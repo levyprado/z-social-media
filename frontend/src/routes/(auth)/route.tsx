bd6b889dfe2850cd88461ae812b155f3f139a945
Levy Prado <levyprado28@gmail.com>
2025-09-28 22:16:00 -0400

    feat: implement route guard to redirect authenticated users

--- frontend/src/routes/(auth)/route.tsx
+++ frontend/src/routes/(auth)/route.tsx
@@ -1,12 +1,20 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { getUser } from '@/features/auth/queries'
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/(auth)')({
+  beforeLoad: async () => {
+    const user = await getUser()
+
+    if (user) {
+      throw redirect({ to: '/' })
+    }
+  },
   component: RouteComponent,
 })
 
 function RouteComponent() {
   return (
-    <div className='min-h-dvh px-4 grid place-items-center'>
+    <div className='grid min-h-dvh place-items-center px-4'>
       <Outlet />
     </div>
   )
