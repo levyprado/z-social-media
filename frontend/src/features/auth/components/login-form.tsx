638c64acc196bc8cd91b01c83c9fe3d3fa428a1b
Levy Prado <levyprado28@gmail.com>
2025-09-29 13:33:14 -0400

    fix: Use navigate() to ensure successful login/signup redirection

--- frontend/src/features/auth/components/login-form.tsx
+++ frontend/src/features/auth/components/login-form.tsx
@@ -4,13 +4,14 @@ import { PasswordField } from '@/components/form/password-field'
 import { Button } from '@/components/ui/button'
 import { LoginSchema } from '@/shared/types'
 import { useForm } from '@tanstack/react-form'
-import { redirect } from '@tanstack/react-router'
+import { useNavigate } from '@tanstack/react-router'
 import { Loader2Icon } from 'lucide-react'
 import { useState } from 'react'
 import { authClient } from '../auth-client'
 
 export default function LoginForm() {
   const [errorMessage, setErrorMessage] = useState('')
+  const navigate = useNavigate({ from: '/login' })
   const form = useForm({
     defaultValues: {
       username: '',
@@ -22,7 +23,7 @@ export default function LoginForm() {
     onSubmit: async ({ value: data }) => {
       await authClient.signIn.username(data, {
         onSuccess: () => {
-          redirect({ to: '/' })
+          navigate({ to: '/' })
         },
         onError: (ctx) => {
           setErrorMessage(ctx.error.message || 'An unexpected error occurred')
--- frontend/src/features/auth/components/signup-form.tsx
+++ frontend/src/features/auth/components/signup-form.tsx
@@ -4,13 +4,14 @@ import { PasswordField } from '@/components/form/password-field'
 import { Button } from '@/components/ui/button'
 import { SignupSchema } from '@/shared/types'
 import { useForm } from '@tanstack/react-form'
-import { redirect } from '@tanstack/react-router'
+import { useNavigate } from '@tanstack/react-router'
 import { Loader2Icon } from 'lucide-react'
 import { useState } from 'react'
 import { authClient } from '../auth-client'
 
 export default function SignupForm() {
   const [errorMessage, setErrorMessage] = useState('')
+  const navigate = useNavigate({ from: '/signup' })
   const form = useForm({
     defaultValues: {
       name: '',
@@ -23,11 +24,12 @@ export default function SignupForm() {
       onChange: SignupSchema,
     },
     onSubmit: async ({ value }) => {
-      const { confirmPassword: _, ...data } = value
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { confirmPassword, ...data } = value
 
       await authClient.signUp.email(data, {
         onSuccess: () => {
-          redirect({ to: '/' })
+          navigate({ to: '/' })
         },
         onError: (ctx) => {
           setErrorMessage(ctx.error.message || 'An unexpected error occurred')
