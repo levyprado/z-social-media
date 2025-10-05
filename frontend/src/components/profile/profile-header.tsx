e810b97baddf48b88678df6df60a2957da6a0cad
Levy Prado <levyprado28@gmail.com>
2025-10-04 22:30:38 -0400

    feat: Implement back navigation in profile header

--- frontend/src/components/profile/profile-header.tsx
+++ frontend/src/components/profile/profile-header.tsx
@@ -1,4 +1,5 @@
 import IconButton from '@/components/icon-button'
+import { useRouter } from '@tanstack/react-router'
 import { ChevronLeftIcon } from 'lucide-react'
 
 type ProfileHeaderProps = {
@@ -10,9 +11,19 @@ export default function ProfileHeader({
   title,
   description,
 }: ProfileHeaderProps) {
+  const router = useRouter()
+
+  const handleGoBack = () => {
+    if (router.history.canGoBack()) {
+      router.history.back()
+    } else {
+      router.navigate({ to: '/' })
+    }
+  }
+
   return (
     <div className='bg-background/85 shadow-xs dark:bg-background/65 sticky left-0 top-0 z-10 flex h-14 w-full items-center gap-4 px-4 backdrop-blur-md'>
-      <IconButton icon={ChevronLeftIcon} size='lg' />
+      <IconButton onClick={handleGoBack} icon={ChevronLeftIcon} size='lg' />
       <div className='flex flex-col'>
         <span className='text-xl font-bold leading-tight'>{title}</span>
         {description && (
