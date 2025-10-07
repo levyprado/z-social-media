8075b81469089587e78756a4122fa8c3522db658
Levy Prado <levyprado28@gmail.com>
2025-10-06 20:07:28 -0400

    feat: Create base posts schema

---
+++ server/db/schema/posts.ts
@@ -0,0 +1,25 @@
+import {
+  integer,
+  pgTable,
+  text,
+  timestamp,
+  type AnyPgColumn,
+} from 'drizzle-orm/pg-core'
+import { user } from './auth'
+
+export const posts = pgTable('posts', {
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  content: text('content').notNull(),
+  userId: text('user_id')
+    .notNull()
+    .references(() => user.id),
+  parentPostId: integer('parent_post_id').references(
+    (): AnyPgColumn => posts.id,
+  ),
+  replyCount: integer('reply_count').default(0).notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at')
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+})
